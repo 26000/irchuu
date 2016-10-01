d362e15fba8ee7f09756f9fd2ac37495b6dea2e6
Snowflake <n264832@yahoo.co.jp>
2016-10-01 18:31:10 +0400

    Implement joining invite-only channels, add error reporting

--- README.md
+++ README.md
@@ -4,30 +4,30 @@ A Telegram<->IRC transport.
 ## Roadmap
 - [x] Basics: config reading
 
-- [-] Joining IRC channels: join, wait for an invite, join with a password
-- [x] Joining Telegram group: leave if not in config
+- [x] Joining IRC channels: join, wait for an invite, join with a password
+- [x] Joining Telegram group: leave wrong groups
 
 - [x] Relaying IRC messages to Telegram
 - [x] Relaying Telegram messages to IRC
 - [ ] Coloured nicknames
 - [ ] Preserve formatted text
 - [ ] Replies, forwards support
-- [ ] Pictures: server and Imgur uploads
-- [ ] Documents: server and uploading
+- [ ] Documents: serving them as links in IRC
+- [ ] Pictures: serving and Imgur uploads
 - [ ] Moderation of the Telegram group from IRC
 - [ ] Moderation of the IRC channel from Telegram
 
 - [ ] Logging to a file
 - [ ] Showing history in IRC on query
 
-- [ ] Showing user list from IRC to TG
-- [ ] Showing moderator list from TG to IRC
-- [ ] Stickers from IRC
+- [ ] Showing user list from IRC in TG
+- [ ] Showing moderator list from TG in IRC
+- [ ] Sending stickers from IRC
 - [ ] Converting emojis
 - [ ] Ability to contact IRC bots
 - [ ] Edits support
 - [ ] Sed from IRC
 
 - [ ] WeeChat plugin
-- [ ] Whois realname from IRC
+- [ ] Ability to get the Telegram user realname from IRC
 - [ ] Sending files through IRC (DCC)
--- irc/irc.go
+++ irc/irc.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 )
 
+// Launch starts the IRC bot and waits for messages.
 func Launch(c *config.Irc, wg *sync.WaitGroup, r *relay.Relay) {
 	defer wg.Done()
 
@@ -19,13 +20,22 @@ func Launch(c *config.Irc, wg *sync.WaitGroup, r *relay.Relay) {
 	irchuu.Password = c.Password
 	irchuu.Log = logger
 
-	irchuu.ReplaceCallback("CTCP_VERSION", 0, func(event *irc.Event) {
+	go logErrors(logger, irchuu.ErrorChan())
+
+	irchuu.ReplaceCallback("CTCP VERSION", 0, func(event *irc.Event) {
 		logger.Printf("Incoming CTCP_VERSION from %v\n", event.Nick)
-		irchuu.Noticef(event.Nick, "\001VERSION IRChuu! v%v (https://github.com/26000/irchuu), based on go-ircevent\001", config.VERSION)
+		irchuu.Noticef(event.Nick, "\001VERSION IRChuu! v%v (https://github.com/26000/irchuu), based on %v\001", config.VERSION, irc.VERSION)
 	})
 
 	irchuu.AddCallback("CTCP", func(event *irc.Event) {
-		logger.Printf("Incoming unknown CTCP from %v\n", event.Nick)
+		logger.Printf("Incoming unknown CTCP %v from %v\n", event.Arguments[1], event.Nick)
+	})
+
+	irchuu.AddCallback("INVITE", func(event *irc.Event) {
+		logger.Printf("Invited to %v by %v\n", event.Arguments[1], event.Nick)
+		if c.Channel == event.Arguments[0] {
+			irchuu.Join(fmt.Sprintf("#%v %v", c.Channel, c.ChanPassword))
+		}
 	})
 
 	irchuu.AddCallback("PRIVMSG", func(event *irc.Event) {
@@ -37,16 +47,58 @@ func Launch(c *config.Irc, wg *sync.WaitGroup, r *relay.Relay) {
 		}
 	})
 
+	// Errors
+	irchuu.AddCallback("461", func(event *irc.Event) {
+		logger.Printf("Error: ERR_NEEDMOREPARAMS\n")
+	})
+
+	irchuu.AddCallback("473", func(event *irc.Event) {
+		logger.Printf("The channel is invite-only, please invite me\n")
+	})
+
+	irchuu.AddCallback("471", func(event *irc.Event) {
+		logger.Printf("The channel is full\n")
+	})
+
+	irchuu.AddCallback("403", func(event *irc.Event) {
+		logger.Printf("The channel doesn't exist\n")
+	})
+
+	irchuu.AddCallback("474", func(event *irc.Event) {
+		logger.Printf("%v is banned on the channel\n", irchuu.GetNick())
+	})
+
+	irchuu.AddCallback("475", func(event *irc.Event) {
+		logger.Printf("The channel password is incorrect\n")
+	})
+
+	irchuu.AddCallback("476", func(event *irc.Event) {
+		logger.Printf("Error: ERR_BADCHANMASK\n")
+	})
+
+	irchuu.AddCallback("405", func(event *irc.Event) {
+		logger.Printf("Error: ERR_TOOMANYCHANNELS\n")
+	})
+
 	err := irchuu.Connect(fmt.Sprintf("%v:%d", c.Server, c.Port))
 	if err != nil {
-		logger.Printf("Cannot connect: %v", err)
+		logger.Printf("Cannot connect: %v\n", err)
 	}
 	irchuu.Join(fmt.Sprintf("#%v %v", c.Channel, c.ChanPassword))
 	go relayMessagesToIRC(r, c, irchuu)
 }
 
+// relayMessagesToIRC listens to the Telegram channel and sends every message
+// into IRC.
 func relayMessagesToIRC(r *relay.Relay, c *config.Irc, irchuu *irc.Connection) {
 	for message := range r.TeleCh {
 		irchuu.Privmsgf("#"+c.Channel, "<%v> %v", message.Nick, message.Text)
+	}
+}
+
+// logErrors listens to an error channel and logs errors.
+func logErrors(logger *log.Logger, ch chan error) {
+	for e := range ch {
+		logger.Printf("Error: %v\n", e)
 	}
 }
