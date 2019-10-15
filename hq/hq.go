c9bc02f96f16b452644781e82f680c11599181ba
Snowflake <notme@airmail.cc>
2019-10-15 10:05:08 +0000

    Refactor HQ

--- hq/hq.go
+++ hq/hq.go
@@ -19,54 +19,64 @@ const URI = "https://26000.github.io/irchuu/version.json"
 
 // Report checks for a new version sending data if enabled.
 func Report(irchuu *config.Irchuu, tg *config.Telegram, irc *config.Irc) {
-	if irchuu.CheckUpdates || irchuu.SendStats {
-		resp, err := http.Get(URI)
-		if err != nil {
-			log.Printf("Failed to connect to HQ entrance (check for updates and/or share stats): %v.\n",
-				err)
-			return
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Printf("Failed to connect to HQ entrance (check for updates and/or share stats): %v.\n",
-				err)
-			return
-		}
+	if !irchuu.CheckUpdates && !irchuu.SendStats {
+		return
+	}
 
-		arr := make([]string, 4)
-		err = json.Unmarshal(body, &arr)
-		if err != nil {
-			log.Printf("HQ entrance is crazy! Could not check for updates: %v.\n", err)
-		}
+	resp, err := http.Get(URI)
+	if err != nil {
+		log.Printf("Failed to connect to HQ entrance (check for updates and/or share stats): %v.\n",
+			err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to connect to HQ entrance (check for updates and/or share stats): %v.\n",
+			err)
+		return
+	}
 
-		layer, err := strconv.Atoi(arr[0])
-		if err != nil {
-			log.Printf("failed to decode current layer: %v.\n", err)
-			return
-		}
-		if layer > config.LAYER {
-			log.Println("New version available, please check https://github.com/26000/irchuu\n  or use `go get -u github.com/26000/irchuu` if using go < 1.11, `go get github.com/26000/irchuu` otherwise")
-		} else {
-			log.Println("Using the latest version of IRChuu.")
-		}
+	arr := make([]string, 4)
+	err = json.Unmarshal(body, &arr)
+	if err != nil {
+		log.Printf("HQ entrance is crazy! Could not check for updates: %v.\n", err)
+	}
 
-		if irchuu.SendStats {
-			tgHash := sha256.Sum256([]byte(strconv.FormatInt(tg.Group, 10)))
-			ircHash := sha256.Sum256([]byte(irc.Channel))
+	layer, err := strconv.Atoi(arr[0])
+	if err != nil {
+		log.Printf("Failed to decode current layer: %v.\n", err)
+		return
+	}
+	if layer > config.LAYER {
+		log.Println("New version available, please check https://github.com/26000/irchuu\n  or use `go get -u github.com/26000/irchuu` if using go < 1.11, `go get github.com/26000/irchuu` otherwise")
+	} else {
+		log.Println("Using the latest version of IRChuu.")
+	}
 
-			// not nice, but relatively readable...
-			resp, err := http.Post(arr[1], "application/json",
-				bytes.NewReader([]byte(fmt.Sprintf(`{ "text": "launched with tg: %v, irc: %v, layer: %v",
+	if irchuu.SendStats {
+		sendTelemetry(arr[1], strconv.FormatInt(tg.Group, 10), irc.Channel)
+	}
+}
+
+func sendTelemetry(uri string, tgGroup string, ircChannel string) {
+	if uri == "" {
+		return
+	}
+
+	tgHash := sha256.Sum256([]byte(tgGroup))
+	ircHash := sha256.Sum256([]byte(ircChannel))
+
+	// not nice, but relatively readable...
+	resp, err := http.Post(uri, "application/json",
+		bytes.NewReader([]byte(fmt.Sprintf(`{ "text": "launched with tg: %v, irc: %v, layer: %v",
 				"format": "plain", "displayName": "IRChuu~" }`,
-					base64.StdEncoding.EncodeToString(tgHash[:31]),
-					base64.StdEncoding.EncodeToString(ircHash[:31]),
-					config.LAYER))))
-			if err != nil {
-				log.Printf("Failed to send usage stats: %v.\n", err)
-			} else if resp != nil && resp.Body != nil {
-				resp.Body.Close()
-			}
-		}
+			base64.StdEncoding.EncodeToString(tgHash[:31]),
+			base64.StdEncoding.EncodeToString(ircHash[:31]),
+			config.LAYER))))
+	if err != nil {
+		log.Printf("Failed to send usage stats: %v.\n", err)
+	} else if resp != nil && resp.Body != nil {
+		resp.Body.Close()
 	}
 }
